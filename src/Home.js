8c32606ef08c9a8364a96a48153c50e540933f6c
Tectonic-06 <106272505+Tectonic-06@users.noreply.github.com>
2024-10-23 05:12:01 +0100

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+function Home() {
+    return (
+        <div>
+            <div className='head'>
+                <h1>Home Page</h1>
+                <nav className='nav'>
+                    <Link to="/"><p>Home |</p></Link>
+                    <Link to="/about"><p>About |</p></Link>
+                    <Link to="/services"><p>Services |</p></Link> 
+                    <Link to="/contact"><p>Contact </p></Link>
+                </nav>
+            </div>
+            <p className='text'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius veniam atque quis debitis tempore culpa iste illo natus, in soluta, aspernatur consequatur adipisci eos sit dolores tenetur nulla fuga delectus? Quidem, atque commodi fugit exercitationem eligendi iste sint, similique eaque non accusamus minus, veniam aspernatur. Exercitationem vitae accusamus placeat laboriosam tenetur eos, maxime ab laborum fugiat quidem illum eaque obcaecati corrupti dolores. Culpa cumque repellendus, mollitia, dolores quia at quod voluptatibus, nulla itaque commodi nesciunt officiis facere ipsa. Tempora cupiditate ipsum incidunt similique dolor natus numquam, sapiente nisi harum iste in mollitia culpa inventore illo exercitationem! Quibusdam at, voluptatem, ullam, quam qui necessitatibus minus molestiae autem dignissimos iste adipisci possimus enim culpa distinctio nam eos aspernatur exercitationem! Obcaecati quam deserunt qui dicta quidem aliquid hic maxime numquam fuga assumenda natus nemo blanditiis sint quos voluptas accusamus ratione quis veritatis aut, enim, reprehenderit laudantium nostrum. Rerum nulla beatae omnis, fugit eum maiores deserunt neque minus recusandae nobis illo provident cumque quas placeat? Alias distinctio rem praesentium dolorum, doloribus harum error illum dolores possimus, fugit voluptatem natus iusto repellendus hic voluptates minus doloremque, a officiis? Autem cupiditate, dignissimos dolorum voluptate in itaque. Consequatur voluptate porro reprehenderit voluptates accusantium et officia eos nemo unde animi aliquam vel eveniet hic omnis libero ducimus odio provident quos laborum quo autem, voluptatum fugit. Officia saepe quam, quidem totam reprehenderit eius ullam unde itaque ducimus quos, veritatis minima facilis labore. Doloribus voluptatibus, vel itaque eos corrupti voluptatum illum numquam aperiam officiis incidunt dolorum perferendis id voluptates ipsam eaque ullam dolor. Nisi laudantium odio ab ex voluptatem corporis saepe earum quibusdam hic atque quaerat explicabo, temporibus et totam eius dolor, fugiat maiores, ipsa commodi ipsam nulla. Ducimus modi ratione fuga ipsum sint cum cumque eos? Voluptates sunt veniam quasi in amet odit maxime aspernatur vel id itaque corrupti eveniet, officiis quia officia accusamus porro consequatur eos incidunt, dolores nostrum hic! Qui incidunt et blanditiis debitis ab doloribus, odio commodi soluta similique voluptatibus, tempore impedit aperiam, veritatis ipsum. Ullam, expedita. Eligendi alias illo veritatis necessitatibus quaerat rem magnam natus accusantium, maxime repudiandae iste. Totam ipsum repudiandae distinctio explicabo eius!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
